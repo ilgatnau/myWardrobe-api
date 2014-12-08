37e076640dec002b74315e98e25cd77a545e37a1
Claudia <ilgatnau@gmail.com>
2014-12-08 22:38:53 +0100

    How to update entity relationships

--- src/main/java/com/gatnau/mywardobe/api/users/UserRepository.java
+++ src/main/java/com/gatnau/mywardobe/api/users/UserRepository.java
@@ -3,7 +3,15 @@ package com.gatnau.mywardobe.api.users;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+/**
+	How to manage resouces relationships
+	- http://stackoverflow.com/questions/17981252/how-to-update-reference-object-in-spring-data-rest?rq=1
+	- http://docs.spring.io/spring-data/rest/docs/2.2.0.RELEASE/reference/html/#repository-resources.association-resource	
+	- http://stackoverflow.com/questions/22499819/spring-data-rest-update-a-resource%C2%B4s-association-using-proper-http-method
+*/
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
 
 }
