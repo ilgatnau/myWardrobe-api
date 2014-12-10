dea6c7907313a6f80bd593ca421e990faead7233
Ivan L. Gatnau <ilgatnau@gmail.com>
2014-12-10 09:54:06 +0100

    PATCH relationships example

--- src/main/java/com/gatnau/mywardobe/api/users/UserRepository.java
+++ src/main/java/com/gatnau/mywardobe/api/users/UserRepository.java
@@ -6,10 +6,18 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 /**
-	How to manage resouces relationships
+	How to manage resources relationships
 	- http://stackoverflow.com/questions/17981252/how-to-update-reference-object-in-spring-data-rest?rq=1
 	- http://docs.spring.io/spring-data/rest/docs/2.2.0.RELEASE/reference/html/#repository-resources.association-resource	
 	- http://stackoverflow.com/questions/22499819/spring-data-rest-update-a-resource%C2%B4s-association-using-proper-http-method
+	
+	PATCH relationship example: 
+		{"wardrobes" : 
+ 			[
+ 				"http://localhost:8080/wardrobes/1", 
+ 				"http://localhost:8080/wardrobes/2"
+ 			]
+		}
 */
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
